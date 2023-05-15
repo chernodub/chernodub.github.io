6f4a74f5a9b42bef8fcad7f69ddcc0d55d8de5b1
Viktor Chernodub <chernodubv@gmail.com>
2023-05-15 22:22:12 +0200

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'; // https://astro.build/config
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chernodub.github.io',
+  integrations: [tailwind()]
 });
